facbfccad07a647c581a95d50b59f8cb031c0c54
JabezJesudasonJena <boycalledjabez@gmail.com>
2025-10-17 13:15:06 +0530

    Some minor changes over catch error

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -4,7 +4,7 @@ const getAllPosts = async (req, res) => {
     try {
         const posts = await Post.find({}).sort({ createdAt : -1});
         res.status(200).json(posts);
-    } catch (error) {
+    } catch (error) {   
         res.status(500).json({message : "Server Error"});
     }
 }
