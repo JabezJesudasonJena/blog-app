34e070a8532529afc8950296ad8d9ae28fdd7d5b
JabezJesudasonJena <170162059+JabezJesudasonJena@users.noreply.github.com>
2025-10-21 23:53:18 +0530

    Add edit and delete post functionality to PostPage component

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const PostPage = () => {
     const [post, setPost] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const {id} = useParams();
+    const navigate = useNavigate();
 
     useEffect( () => {
         const fetchPosts = async () => {
@@ -23,6 +25,17 @@ const PostPage = () => {
         fetchPosts();
     },[id]);
 
+    const handleDelete = async () => {
+        try {
+            await axios.delete(`http://localhost:5000/api/posts/${id}`);
+            console.log("Deleted : " + id);
+            navigate('/allposts')
+        }
+        catch (error) {
+            console.log("The error Appeared : " + error );
+        }
+    }
+
     if (isLoading) {
         return <p>Loading Post...</p>
     }
@@ -33,13 +46,20 @@ const PostPage = () => {
 
 
     return (
-        <article>
+        <article className="flex flex-col justify-center items-center ">
             <h1>{post.title}</h1>
             <p>
                 {/* Posted on {new Date(new (post.createdAt). toLocalDateString())} */}
             </p>
             <div>
                 <p>{post.content}</p>
+                <button className="w-[100px] h-[40px] border-[2px] m-t-[10px] cursor-pointer mt-[10px]  font-bold " onClick={handleDelete}>Delete Post</button>
+                
+                <Link 
+                className="w-[200px] h-[40px] border-[2px] m-t-[10px] cursor-pointer mt-[10px]  font-bold "
+                to={`/editpost/${post.id}`}>
+                    Edit
+                </Link>
             </div>
         </article>
     )
